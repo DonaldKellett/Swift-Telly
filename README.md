60f12b993d374ef57bdaaca0c244938bcc6b3acc
DonaldKellett <i.donaldl@me.com>
2016-06-29 12:23:21 +0800

    Add TV Files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Swift-Telly
+
 A simple demonstration of subviews and animations in Swift.  Public Domain
+
+## Version Details
+
+- Current Version Number: None
+- Status: Ready for demonstration purposes.  Not suited for production.
+- License: **Public Domain**
---
+++ Swift Telly/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Swift Telly
+//
+//  Created by Donald Sebastian Leung on 2016/6/29.
+//  Copyright © 2016年 Donald Sebastian Leung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var tvScreen: UIView!
+    @IBAction func turnOnTheTelly(s: AnyObject) {
+        let tvScreenOn = UIView(frame: CGRectMake(120, 80, 1, 1))
+        tvScreenOn.backgroundColor = UIColor.whiteColor()
+        tvScreen.addSubview(tvScreenOn)
+        UIView.animateWithDuration(1.0, animations: {
+            tvScreenOn.transform = CGAffineTransformMakeScale(240, 1)
+        })
+        UIView.animateWithDuration(1.0, animations: {
+            UIView.setAnimationDelay(1.0)
+            tvScreenOn.transform = CGAffineTransformConcat(tvScreenOn.transform, CGAffineTransformMakeScale(1, 160))
+        })
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let tvBody = UIView(frame: CGRectMake(75, 250, 280, 200))
+        tvBody.backgroundColor = UIColor.brownColor()
+        self.view.addSubview(tvBody)
+        let tvLeftAntenna = UIView(frame: CGRectMake(70, -90, 3, 100))
+        tvLeftAntenna.backgroundColor = UIColor.blackColor()
+        tvLeftAntenna.transform = CGAffineTransformMakeRotation(-0.5)
+        tvBody.addSubview(tvLeftAntenna)
+        let tvRightAntenna = UIView(frame: CGRectMake(210, -90, 3, 100))
+        tvRightAntenna.backgroundColor = UIColor.blackColor()
+        tvRightAntenna.transform = CGAffineTransformMakeRotation(0.5)
+        tvBody.addSubview(tvRightAntenna)
+        tvScreen = UIView(frame: CGRectMake(20, 20, 240, 160))
+        tvScreen.backgroundColor = UIColor.blackColor()
+        tvBody.addSubview(tvScreen)
+    }
+}
+
